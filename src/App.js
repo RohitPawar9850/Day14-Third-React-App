b07fdc74defea380acac3458daf476562153a5e3
Rohit Pawar <friends9850@gmail.com>
2023-06-14 20:08:34 +0530

    String conversion from upper to lower

--- src/App.js
+++ src/App.js
@@ -4,37 +4,34 @@ function App()
 {
   return (
     <>
-    <div className="bg-dark text-primary p-3">
-    <h1 className="bg-info text-primary p-3">Counter App1</h1>
-    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque expedita dolor tempore consequatur magni fugit necessitatibus architecto, recusandae aliquid, iure eum molestiae repellendus placeat maxime! Similique alias architecto voluptates explicabo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias vero modi explicabo facere! Error, dolor quo ipsam expedita harum fuga consectetur necessitatibus voluptas illum excepturi, repellat consequatur asperiores aut deserunt. </div>
-    <CounterApp> </CounterApp>
-    <hr />
-    <h1 className="bg-info text-primary p-3">Counter App2</h1>
-    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque expedita dolor tempore consequatur magni fugit necessitatibus architecto, recusandae aliquid, iure eum molestiae repellendus placeat maxime! Similique alias architecto voluptates explicabo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias vero modi explicabo facere! Error, dolor quo ipsam expedita harum fuga consectetur necessitatibus voluptas illum excepturi, repellat consequatur asperiores aut deserunt. </div>
-    <CounterApp> </CounterApp>
-    </div>
+    <h1>String Demo</h1>
+    <StringDemo></StringDemo>
     </>
   );
 }
 
-function CounterApp()
+function StringDemo()
 {
-  // let list = useState(1);
-  // let [counter,setCounter] = list;
-  let [counter,setCounter] = useState(1); //stateful + DOM opr
+  let [title,SetTitle] =useState ("Hello World");
+  let UpperCaseAction = () => {
+    title = title.toUpperCase();
+    console.log(title);
+    SetTitle(title);
 
-  let LikeMeAction = () => {
-    counter = counter + 1 ;
-    console.log(counter);
-    setCounter(counter);
   };
-  return (
-    <>
-    <h1>{counter}{""}</h1>
-    <br />
-    <input type="button" value="&#128077;" onClick={LikeMeAction} />
-    </>
-  );
+  let LowerCaseAction = () => {
+    title = title.toLowerCase();
+    console.log(title);
+    SetTitle(title);
+  };
+
+ return (
+  <>
+  <h1>{title}</h1>
+  <input type="button" value="LowerCase" onClick={LowerCaseAction} />
+  <input type="button" value="UpperCase" onClick={UpperCaseAction} />
+  </>
+ );
 }
 
 export default App;
