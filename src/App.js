7202094f8fd576a917b675805e4b91fbbbd1fb60
Rohit Pawar <friends9850@gmail.com>
2023-06-14 11:50:47 +0530

    header body footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <>
-    <h1>
-      Hello World
-    </h1>
+    <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+    </>
+  );
+}
+function Header()
+{
+  return (
+    <>
+    <h1 className='bg-primary'>Header</h1>
+    <h2 className='bg-info'>Header</h2>
+    </>
+  );
+}
+
+function Body()
+{
+  return (
+    <>
+    <h1 className='bg-dark text-light'>body</h1>
+    </>
+  );
+}
+
+function Footer()
+{
+  return (
+    <>
+    <h1 className='bg-primary text-light'>Footer</h1>
     </>
   );
 }
